97ddfd0b8822c20326ee1b242470af1b89df1d18
uguisousa <guilhermehssousa872@gmail.com>
2024-05-21 18:37:56 -0300

    UpG - website: Panda Filme > Epic Filmes

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        var navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+            navbar.classList.add("navbar-scrolled");
+        } else {
+            navbar.classList.remove("navbar-scrolled");
+        }
+    });
+});
